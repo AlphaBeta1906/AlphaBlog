28c24416e37127ee2e9db9265980d2d38b94c2a8
fariz <farizi1906@gmail.com>
2022-07-20 09:01:45 +0700

    updates

--- django_cms/settings.py
+++ django_cms/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = str(getenv("DEBUG"))
 
 ALLOWED_HOSTS = ["*"]
 
