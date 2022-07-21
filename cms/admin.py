8fac2c0c8b4f981f6f31054c97103ab4ba1c2d07
fariz <farizi1906@gmail.com>
2022-07-21 10:44:53 +0700

    prevent error when migrate

--- cms/admin.py
+++ cms/admin.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib import admin
 from django.forms import ModelChoiceField, ModelForm
+from django.db.utils import ProgrammingError
 from .models import Post, Tag,SiteSettings
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
@@ -10,7 +12,19 @@ class TagChoiceField(ModelChoiceField):
     def label_from_instance(self, obj):
         return str(obj.tag_name)
 
+class AdminPostForm(ModelForm):
     tag = forms.ChoiceField(choices=[])
+    def __init__(self, *args, **kwargs):
+        try:
+            _tags = [(_tag["tag_name"],_tag["tag_name"]) for _tag in Tag.objects.values("tag_name")]
+        except ProgrammingError:
+            _tags = [("","")]    
+        super(AdminPostForm, self).__init__(*args, **kwargs)
+        self.fields['tag'].choices = _tags
+    
+    class Meta:
+        model  = Post
+        fields = ("author","title","content","cover_image","date_update","tag","draft")
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title","author","tag","slug","date_publish","date_update","draft")
