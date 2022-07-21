1921b413676747fba6ec6ccd50ee703d451f650f
fariz <farizi1906@gmail.com>
2022-07-21 11:03:09 +0700

    remove markdownx

--- django_cms/urls.py
+++ django_cms/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("markdownx/", include("markdownx.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("summernote/", include("django_summernote.urls")),
