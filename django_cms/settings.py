1ca08831e1ec1fa53e6c1f4e9249f14f7cead170
fariz <farizi1906@gmail.com>
2022-07-23 10:31:55 +0700

    updates

--- django_cms/settings.py
+++ django_cms/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = (getenv("DEBUG") == "true")
+DEBUG = (str(getenv("DEBUG")) == "true")
 
 ALLOWED_HOSTS = ["*"]
 
@@ -193,9 +193,9 @@ MARKDOWNIFY = {
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-#STATICFILES_DIRS = (
-#   BASE_DIR / "static",
-#)
+STATICFILES_DIRS = (
+   BASE_DIR / "static",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- django_cms/urls.py
+++ django_cms/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
     #path("summernote/", include("django_summernote.urls")),
     path("markdownx/", include("markdownx.urls")),
     path("", include("blog.urls")),
-]
-
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
